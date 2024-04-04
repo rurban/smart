e3381644ca6fb1010ac8415a8134d1c38c0670a1
Reini Urban <reini.urban@gmail.com>
2024-04-04 20:45:36 +0200

    better best20 candidates

--- best.sh
+++ best.sh
@@ -3,6 +3,6 @@ set -x
 make -s clean
 make -s
 
-./select -backup -none tvsbs fjs hash8 so sbndm sbndm2 sbndm-bmh fndm fsbndm sbndmq8 ufndmq2 ufndmq8 kbndm fs-w2 lwfr5 qf43 sbndm-w2 wfr2 twfrq2 twfrq4 wom libc1 musl1 simdkr epsm
+./select -backup -none tvsbs fjs hash8 so sbndm sbndm2 sbndm-bmh fndm sbndmq8 ufndmq2 dbww dbww2 fs-w2 lwfr5 qf43 sbndm-w2 tsa wfr2 twfrq2 twfrq4 wom libc1 musl1 simdkr epsm
 ./smart -text all -all -txt
 ./select -restore
