e5ef93eb975bcc56b490f86f612997e1fafa6422
Reini Urban <reini.urban@gmail.com>
2025-03-17 13:22:06 +0100

    hashchain: fix -Wmaybe-uninitialized

--- source/algos/lhc3.c
+++ source/algos/lhc3.c
@@ -82,7 +82,7 @@ unsigned int preprocessing(const unsigned char *x, int m, unsigned int *B) {
     for (int i = 0; i < ASIZE; i++) B[i] = 0;
 
     // 1. Calculate all the chain hashes, ending with processing the entire pattern so H has the cumulative value.
-    unsigned int H;
+    unsigned int H = 0;
     int start = m < Q2 ? m - END_FIRST_QGRAM : Q;
     for (int chain_no = start; chain_no >= 1; chain_no--)
     {
